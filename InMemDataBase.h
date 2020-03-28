0339593b3f6d133d628a1484370fade1b7e76556
root <root@LAPTOP-S8ARC5AJ.localdomain>
2020-03-28 16:46:05 +0100

    Databaserna för inmemory lagring

---
+++ InMemDataBase.h
@@ -0,0 +1,41 @@
+#ifndef INMEMDATABASE_H
+#define INMEMDATABASE_H
+
+struct Article {
+  string title;
+  string author;
+  unsigned ID;
+  string text;
+};
+
+struct Newsgroup {
+  string Name;
+  unsigned NewsGroup_ID;
+  unsigned Article_IDs;
+  std::vector<Article> articles;
+};
+
+class InMemDatabase {
+public:
+
+  InMemDatabase();
+  // en enda lång string, messagehandler måste omvandla den till bytes som går att skicka
+  string listNewsGroups();
+
+  string listArticles(string newsgroup);
+  string listArticles(unsigned newsgroup);
+
+  bool createNewsGroup()
+
+private:
+  unsigned NEWSGROUP_ID;
+  std::vector<Newsgroup> newsGroups;
+
+
+
+};
+
+
+
+
+#endif
