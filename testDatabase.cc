d414c7182fe1d2c302223d85eecc67279fd9482e
root <root@LAPTOP-S8ARC5AJ.localdomain>
2020-04-06 22:02:28 +0200

    Fortsatt med databsen, riktigt klurigt det där emd filerna men aja

--- testDatabase.cc
+++ testDatabase.cc
@@ -12,5 +12,9 @@ int main(int argc, char *argv[]){
 
   FileDatabase d;
 
-  d.listNewsgroups();
+  std::vector<Article> arts = d.listArticles(1);
+  for(auto& a : arts){
+    std::cout << "First article:\t" << a.title << "\tauthor:\t" << a.author
+              << std::endl << std::endl << a.text << std::endl;
+  }
 }
