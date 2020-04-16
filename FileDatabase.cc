3603198f01a62c653ba819dd975f32e3c909459b
root <root@LAPTOP-S8ARC5AJ.localdomain>
2020-04-16 16:18:29 +0200

    Fortsatt arbete med FileDatabase

--- FileDatabase.cc
+++ FileDatabase.cc
@@ -11,6 +11,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <cstring>
+#include <cerrno>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -51,6 +53,7 @@ FileDatabase::FileDatabase(){
   std::ofstream manifest("./Database/manifest" , std::ofstream::app); // list of Newsgroups
 
   manifest.close();
+  // Kna inte ha en variable med newsgroup id måste läsa in antal rader i manifest
   NEWSGROUP_ID = 0;
 
 }
@@ -61,20 +64,16 @@ FileDatabase::~FileDatabase(){
 // Borde return vector med strings istället blir asjobbigt annars!
 std::vector<Newsgroup> FileDatabase::listNewsgroups(){
   std::vector<Newsgroup> ngs;
-  //std::unordered_map<int,Article> tmp;
   std::vector<Article> tmpA;
-  unsigned ID = 0;
-  struct dirent* ent;
-  DIR* tmp = nullptr, *tmp2 = nullptr;
+
+
+  DIR* tmp = nullptr;
 
   if((tmp = opendir(root)) == nullptr){
     std::cerr << "Error while opening root directory"  << std::endl;
     exit(1);
   }
-  char source[100];
 
-
-  // Gör om lägg en fil "manifest" längst upp i Database som innehålller info om ngs
   ifstream manifest("./Database/manifest");
   if(!manifest.good()){
     std::cerr << "Error opening manifest file!" << std::endl;
@@ -99,48 +98,115 @@ std::vector<Newsgroup> FileDatabase::listNewsgroups(){
 
 
 bool FileDatabase::createNewsgroup(string name){
-
   ifstream in_manifest("./Database/manifest");
   if(!in_manifest){
-    std::cout << "An error occurred in createNewsgroup while opening manifest." << std::endl;
+    std::cout << "Unable to open manifest file in CreateNewsgroup." << std::endl;
     exit(1);
   }
-  std::string tmpName, tmpId;
+  std::string tmpName, tmpId, artCount;
 
   while(!in_manifest.eof()){
     in_manifest >> tmpName;
     in_manifest >> tmpId;
+    in_manifest >> artCount;
     if(tmpName == name){ // Already exists
       return false;
     }
   }
   in_manifest.close();
 
-  ofstream out_manifest("./Database/manifest", std::ofstream::ate);
+  ofstream out_manifest("./Database/manifest", std::ofstream::app);
   if(!out_manifest){
     std::cout << "An error occurred while opening manifest." << std::endl;
     exit(1);
   }
 
-  NEWSGROUP_ID++; // Make sure always unique
-  out_manifest << name << " " << NEWSGROUP_ID << "\n";
+  NEWSGROUP_ID++;
+  // Structure of manifest file
+  // Newsgroup name newsgroup ID then article counter
+
+  out_manifest << name << " " << NEWSGROUP_ID <<  " 0" << "\n";
   out_manifest.close();
+
+  struct stat sb;
   char dirPath[512] = "./Database/";
   strcat(dirPath, name.c_str());
-  int status = mkdir(dirPath,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
-  if(status == -1){
-    std::cout << "An error occurred while creating newsgroup directory" <<std::endl;
-    exit(1);
+  if(stat(dirPath, &sb) == -1){
+    std::cout << std::strerror(errno) << std::endl;
   }
 
+  if(!S_ISDIR(sb.st_mode)){
+    int status = mkdir(dirPath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if(status == -1){
+      std::cout << "An error occurred while creating newsgroup directory." <<std::endl;
+      exit(1);
+    }
+}
 
   return true;
 }
 
 
 bool FileDatabase::deleteNewsgroup(unsigned ng_ID){
-  return false;
+
+
+  std::fstream manifest("./Database/manifest");
+
+  if(!manifest){
+    // something wrong
+  }
+  manifest.seekg(0,manifest.end);
+  int filelength = manifest.tellg();
+  char* contents = new char[filelength];
+  manifest.seekg(0,manifest.beg);
+
+
+  unsigned tmpID, tmpArtCounter;
+  std::string tmpName,name;
+  unsigned line = 0;
+  // check manifest for newsgroup id
+  while(!manifest.eof()){
+    manifest >> tmpName;
+    manifest >> tmpID;
+    manifest >> tmpArtCounter;
+
+    if(tmpID == ng_ID){
+      break;
+    }else{
+      line++;
+    }
+  }
+  if(tmpID != ng_ID){ // Newsgroup not found
+    return false;
+  }
+
+  name = tmpName;
+
+  manifest.read(contents, filelength);
+  manifest.clear();
+  manifest.seekg(0);
+
+  for(unsigned i=0; i != line;i++){
+    manifest >> tmpName;
+    manifest >> tmpID;
+    manifest >> tmpArtCounter;
+  }
+  manifest.write(contents, filelength);
+
+
+
+  // remove newsgroup line from manifest
+  char path[512] = "./Database/";
+  strcat(path, name.c_str());
+  int status = remove(path);
+
+  std::cout << "Status of deletion operation: " << status << std::endl;
+
+
+  // remove folder
+  delete[] contents;
+  return true;
 }
 
 // Presumes the newsgroup with ng_ID exists
@@ -157,10 +223,11 @@ std::vector<Article> FileDatabase::listArticles(unsigned ng_ID){
     exit(1);
   }
   std::string name;
-  unsigned ID;
+  unsigned ID,artID;
   while(!manifest.eof()){ // Detta inte testat för flera newsgroups
     manifest >> name;
     manifest >> ID;
+    manifest >> artID;
     if(ID == ng_ID)
       break;
   }
@@ -176,10 +243,8 @@ std::vector<Article> FileDatabase::listArticles(unsigned ng_ID){
   }
 
 
-
   std::string title,author,text,tmp;
   unsigned art_ID;
-//  char c;
 
   while((ent = readdir(dir)) != nullptr){
     strcpy(filepath,dirLocation); // ./Database/[NewsgroupName]
@@ -192,6 +257,7 @@ std::vector<Article> FileDatabase::listArticles(unsigned ng_ID){
       std::cout << "An error occurred when reading stats about an article." << std::endl;
       exit(1);
     }
+
     if(!S_ISREG(s.st_mode)){ // file is not a regular file
         continue;
     }
@@ -223,16 +289,99 @@ std::vector<Article> FileDatabase::listArticles(unsigned ng_ID){
   return arts;
 }
 
-// false om inte NG finns
+// Creates a new article in the ng_ID newsgroup
+// Returns false if the newsgroup does not exist
 bool FileDatabase::createArticle(unsigned ng_ID , string title, string author, string text){
 
-  // kolla om NG finns manifest
+  std::ifstream in_manifest("./Database/manifest");
+  if(!in_manifest){
+    std::cout << "An error occurred while opening manifest file in createArticle." <<std::endl;
+    exit(1);
+  }
+  std::string ngName;
+  unsigned ng,artID;
+  while(!in_manifest.eof()){
+    in_manifest >> ngName;
+    in_manifest >> ng;
+    in_manifest >> artID;
+    if(ng == ng_ID){ // found correct
+      break;
+    }
+  }
+  in_manifest.close();
+  if(ng != ng_ID) // not found
+    return false;
 
+  // increase counter of created articles in manifest
+  increaseArtCounter(ng_ID);
+  artID++; // to match increased Article count
 
-  return false;
+  char filePath[512] = "./Database/";
+  strcat(filePath, ngName.c_str());
+  filePath[strlen(filePath)] = '/';
+  std::string tmpArtID = std::to_string(artID);
+  strcat(filePath, tmpArtID.c_str());
+
+  std::ofstream article(filePath, std::ofstream::trunc);
+
+  if(!article){
+    std::cout<< "error opening article for output!"<<std::endl;
+    exit(1);
+  }
+
+  article << title << std::endl;
+  article << author << std::endl;
+  article << tmpArtID << std::endl;
+  article << text;
+
+  return true;
 }
 
 
+// Increase article counter for Newsgroup with ID with one in manifest file
+void FileDatabase::increaseArtCounter(unsigned ID){
+  std::fstream file("./Database/manifest");
+
+  if(!file){std::cout << "Error in increaseartcountrer."<< std::endl;
+    exit(1);
+  }
+  file.seekg(0,file.end);
+  int filelength = file.tellg();
+  char* contents = new char[filelength];
+  file.seekg(0,file.beg);
+
+  std::string str;
+  int tmp2, position = 0;
+  unsigned tmp1;
+  while(!file.eof()){
+    file >> str;
+    file >> tmp1;
+    if(tmp1 == ID){
+      position = file.tellg();
+      position++; // For space
+      file >> tmp2;
+      break;
+    }
+  }
+  file.read(contents, filelength);
+  tmp2++;
+  str = std::to_string(tmp2);
+  char* newContents = new char[filelength];
+  strcat(newContents, str.c_str());
+  strcat(newContents,contents);
+  file.clear();
+
+  if(!file.good()){
+    std::cout << "något är fel!";
+  }
+  file.seekg(position);
+  file.write(newContents, filelength);
+
+  delete[] newContents;
+  delete[] contents;
+
+  file.close();
+}
 bool FileDatabase::deleteArticle(unsigned ng_ID , unsigned art_ID){
   return false;
 }
--- testDatabase.cc
+++ testDatabase.cc
@@ -11,7 +11,15 @@ int main(int argc, char *argv[]){
 
 
   FileDatabase d;
-  std::cout << "Directory created: " <<static_cast<bool> (d.createNewsgroup("Ne ws1"))
+  std::cout << "Directory created: " <<static_cast<bool> (d.createNewsgroup("News1"))
+  << std::endl;
+  std::cout << "Directory created: " <<static_cast<bool> (d.createNewsgroup("News2"))
+  << std::endl;
+  std::cout << "Directory created: " <<static_cast<bool> (d.createNewsgroup("News3"))
+  << std::endl;
+  std::cout << "Article created: " << static_cast<bool>(d.createArticle(1,"title","author","Text text"))
+  << std::endl;
+  std::cout << "Newsgroup deleted: " << static_cast<bool>(d.deleteNewsgroup(1))
   << std::endl;
   //std::vector<Newsgroup> ngs = d.listNewsgroups();
 
