85b8bef2ba2a1c2f93b7e0866305ee0d60db0ac7
mikapersson <perssonmika@gmail.com>
2020-04-17 17:12:10 +0200

    Removed some includes

--- FileDatabase.cc
+++ FileDatabase.cc
@@ -13,16 +13,9 @@
 
 #include <cstring>
 #include <cerrno>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <errno.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <dirent.h>
 
 using std::ofstream;
 using std::ifstream;
