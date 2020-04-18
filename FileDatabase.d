7b54a547406c86c18839225380a5c5696ed5014a
mikapersson <perssonmika@gmail.com>
2020-04-18 13:09:45 +0200

    removed o, d files

--- FileDatabase.d
+++
@@ -1,5 +0,0 @@
-FileDatabase.o: FileDatabase.cc FileDatabase.h Database.h
-
-FileDatabase.h:
-
-Database.h:
--- testDatabase.d
+++
@@ -1,5 +0,0 @@
-testDatabase.o: testDatabase.cc FileDatabase.h Database.h
-
-FileDatabase.h:
-
-Database.h:
