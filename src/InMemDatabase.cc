ca0a13c23e0ee0a9edde0ddf81e8a0392c2a7691
mikapersson <perssonmika@gmail.com>
2020-04-26 19:26:32 +0200

    created src dir

---
+++ src/InMemDatabase.cc
@@ -0,0 +1,107 @@
+#include "InMemDatabase.h"
+#include <iostream>
+#include <algorithm>
+
+InMemDatabase::InMemDatabase() {
+  NEWSGROUP_ID = 0;
+}
+
+InMemDatabase::~InMemDatabase(){}
+
+std::vector<Newsgroup> InMemDatabase::listNewsgroups(){
+  std::vector<Newsgroup> ngs;
+  for(auto& n : newsgroups){
+    ngs.push_back(n.second);
+  }
+  std::sort(ngs.begin(),ngs.end(), [&](Newsgroup &n1, Newsgroup &n2){return n1.newsGroup_ID < n2.newsGroup_ID;});
+  return ngs;
+}
+
+bool InMemDatabase::createNewsgroup(string name){
+
+  for(auto& n : newsgroups){
+    if(n.second.name == name){
+      return false;
+    }
+  }
+    NEWSGROUP_ID++; // To make sure that every newsgroup is unique
+    Newsgroup n = {name, NEWSGROUP_ID, 0,std::unordered_map<unsigned,Article>()};
+    newsgroups[NEWSGROUP_ID] = n;
+    return true;
+}
+
+
+bool InMemDatabase::deleteNewsgroup(unsigned ng_ID){
+
+  if(newsgroups.count(ng_ID) == 0)
+    return false;
+
+  newsgroups.erase(ng_ID);
+  return true;
+
+
+}
+
+vector<Article> InMemDatabase::listArticles(unsigned ng_ID){
+  Newsgroup ng = newsgroups[ng_ID];
+  vector<Article> articles;
+  //std::for_each(ng.articles.begin(),ng.articles.end(),[]( std::pair<unsigned,Article> &p){articles.push_back(p.second);});
+
+  for(auto& a : ng.articles){
+    articles.push_back(a.second);
+  }
+
+  std::sort(articles.begin(),articles.end(), [&](Article &a1, Article &a2){return a1.article_ID < a2.article_ID;});
+  return articles;
+}
+
+
+
+bool InMemDatabase::createArticle(unsigned ng_ID , string title, string author, string text){
+    Newsgroup* ng;
+    try{
+        ng = &newsgroups.at(ng_ID);
+    }catch(std::out_of_range e){
+        std::cout << "no ng id\n";
+        return false;
+    }
+    ng->article_IDs++;
+    ng->articles.insert({ng->article_IDs,Article{title,author,ng->article_IDs,text}});
+
+    return true;
+
+}
+// Utgår från att den går igenom
+bool InMemDatabase::deleteArticle(unsigned ng_ID , unsigned art_ID){
+
+  Newsgroup* ng = &newsgroups.at(ng_ID);
+  ng->articles.erase(art_ID);
+  return true;
+}
+
+
+Article InMemDatabase::getArticle(unsigned ng_ID , unsigned art_ID){
+  return newsgroups.at(ng_ID).articles.at(art_ID);
+}
+
+bool InMemDatabase::ngExists(unsigned ng_ID){
+
+  try{
+    newsgroups.at(ng_ID);
+  }catch(std::out_of_range e){
+    return false;
+  }
+  return true;
+}
+
+
+bool InMemDatabase::artExists(unsigned ng_ID, unsigned art_ID) {
+  try{
+    newsgroups.at(ng_ID).articles.at(art_ID);
+
+  }catch(std::out_of_range e){
+    return false;
+  }
+
+  return true;
+}
---
+++ src/testDatabase.cc
@@ -0,0 +1,68 @@
+#include "FileDatabase.h"
+#include <iostream>
+#include <vector>
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+
+/*
+  FileDatabase d;
+  std::cout << "Directory created:\t" <<static_cast<bool> (d.createNewsgroup("News12"))
+            << std::endl;
+  std::cout << "Article created:\t" <<(d.createArticle(1,"Intresting things about","Me and myself", "Text about me"))
+                      << std::endl;
+  Article a = d.getArticle(1,1);
+  std::cout << "title:\t" << a.title << "\tauthor:\t"<< a.author
+            << "\n\n" << a.text << std::endl;*/
+
+
+  //std::cout << "Article deleted:\t" <<(d.deleteArticle(1,1))
+              //                  << std::endl;
+
+  /*
+  std::cout << "Directory created: " <<static_cast<bool> (d.createNewsgroup("News2"))
+  << std::endl;
+  std::cout << "Directory created: " <<static_cast<bool> (d.createNewsgroup("News3"))
+  << std::endl;
+  std::cout << "Article created: " << static_cast<bool>(d.createArticle(1,"title","author","Text text"))
+  << std::endl;
+  //std::cout << "Newsgroup deleted: " << static_cast<bool>(d.deleteNewsgroup(1))
+  //<< std::endl;
+  //std::vector<Newsgroup> ngs = d.listNewsgroups();
+  std::cout << "Should exist: " << d.ngExists(1) << std::endl;
+  std::cout << "Should not exist: " << d.ngExists(6) << std::endl;
+  std::cout << "Should exist: " << d.artExists(1,1) << std::endl;
+  std::cout << "should not exist: " << d.artExists(2,1) << std::endl;
+  */
+    /*
+  std::vector<Article> arts = d.listArticles(1);
+  for(auto& a : arts){
+    std::cout << "First article:\t" << a.title << "\tauthor:\t" << a.author
+              << std::endl << std::endl << a.text << std::endl;
+  }
+  */
+
+//MIKA TESTAR
+cout << "Create FileDatabase object\n" << endl;
+FileDatabase database;
+
+database.createNewsgroup("first-NG");
+database.createNewsgroup("second-NG");
+database.createNewsgroup("third-NG");
+database.createNewsgroup("fourth-NG");
+
+database.deleteNewsgroup(2);
+database.deleteNewsgroup(4);
+
+database.createArticle(1, "first-article", "Mika", "This is some other text");
+
+auto list = database.listNewsgroups();
+
+
+}
